979193458652156bd6a86af36c29d6505efaf99e
ztc <1608864756@qq.com>
2024-11-01 22:07:50 +0800

    release: 2.0.13

--- scripts/updateChangelog.js
+++ scripts/updateChangelog.js
@@ -11,10 +11,17 @@ const outputPath = path.join(
   __dirname,
   "../docs/zebra-ui-docs/content/changedlog.md"
 );
+const copyPath = path.join(__dirname, "../src/changelog.md");
 
 async function updateChangelog() {
   try {
+    // 读取 changelog 文件
     const changelogData = await fs.readFile(changelogPath, "utf8");
+
+    // 复制内容到 src/changelog.md
+    await fs.writeFile(copyPath, changelogData, "utf8");
+    signale.success("成功复制 changelog 到 src/changelog.md");
+
     const outputData = await fs.readFile(outputPath, "utf8");
 
     const versionWithDateRegex = /##\s+(\d+\.\d+\.\d+)\s*（.*?）/g;
